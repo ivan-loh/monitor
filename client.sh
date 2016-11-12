0f88f3aa40335dcf4266738ea3ecadcd3e93a421
Ivan Loh <ivan@eriad.com>
2016-11-12 13:40:50 +0800

    more stats

--- client.sh
+++ client.sh
@@ -9,6 +9,7 @@ server=`hostname`
 apikey=<auth apikey>
 endpoint=<endpoint url>
 
+
 # load
 load0=`cat /proc/loadavg | awk '{print $1}'`
 load1=`cat /proc/loadavg | awk '{print $2}'`
@@ -19,6 +20,25 @@ tot=$(awk '{ if (/MemTotal:/) {print $2} }' </proc/meminfo)
 free=$(awk '{ if (/MemFree:/) {print $2} }' </proc/meminfo)
 pers=$(echo "scale=0; 100 - 100 * $free / $tot" | bc)
 
-json="{\"host\": \"$server\", \"load0\": $load0, \"load1\": $load1, \"load2\": $load2, \"memory\": $pers }"
+# others
+timezone=`cat /etc/timezone`
+since=`uptime -s`
+uptime=`uptime -p`
+servertime=`date`
 
-curl -k -H "Content-Type: application/json" -H "x-apikey: $apikey" -X POST -d "$json" '$endpoint' 
+
+json="{
+        \"host\": \"$server\",
+        \"load0\": $load0,
+        \"load1\": $load1,
+        \"load2\": $load2,
+        \"memory_total\": \"$tot kB\",
+        \"memory_free\": \"$free kB\",
+        \"memory_free\": $pers,
+        \"up_since\": \"$since\",
+        \"uptime\": \"$uptime\",
+        \"server_time\": \"$servertime\",
+        \"timezone\": \"$timezone\"
+}"
+
+curl -k -H "Content-Type: application/json" -H "x-apikey: $apikey" -X POST -d "$json" '$endpoint'
